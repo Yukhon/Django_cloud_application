ef538ddf0c52ba660b27ddbd00e94ae8f89b3b07
yla737 <yla737@sfu.ca>
2021-12-10 14:13:59 -0800

    model create

--- notebook/user/models.py
+++ notebook/user/models.py
@@ -7,6 +7,7 @@ class User(models.Model):
     password = models.CharField('password', max_length=32)
     create_time = models.DateTimeField('create_time', auto_now_add=True)
     updated_time = models.DateTimeField('last updated time', auto_now=True)
+    update = 124
 
     def __str__(self):
         return '%s %s %s %s' % (self.username, self.password, self.create_time, self.updated_time)
