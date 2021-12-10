eee5662d6ad06abbcdb223853b73d2e03c7a0f99
liuyukang <1062692035@qq.com>
2021-12-10 12:23:04 -0800

    model create

---
+++ notebook/user/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+# Create your models here.
+class User(models.Model):
+    username = models.CharField('username', max_length=50, unique=True)
+    password = models.CharField('password', max_length=32)
+    create_time = models.DateTimeField('create_time', auto_now_add=True)
+    updated_time = models.DateTimeField('last updated time', auto_now=True)
+
+    def __str__(self):
+        return '%s %s %s %s' % (self.username, self.password, self.create_time, self.updated_time)
